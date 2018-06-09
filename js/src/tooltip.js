4a7b38eec425fe38c307deda500effb00e73a585
Johann-S <johann.servoire@gmail.com>
2018-06-09 21:11:05 +0200

    fix(manipulator): increase coverage for manipulator

--- js/src/tooltip.js
+++ js/src/tooltip.js
@@ -1,5 +1,6 @@
 import Data from './dom/data'
 import EventHandler from './dom/eventHandler'
+import Manipulator from './dom/manipulator'
 import Popper from 'popper.js'
 import SelectorEngine from './dom/selectorEngine'
 import Util from './util'
@@ -631,7 +632,7 @@ class Tooltip {
 
     config = {
       ...this.constructor.Default,
-      ...Util.getDataAttributes(this.element),
+      ...Manipulator.getDataAttributes(this.element),
       ...typeof config === 'object' && config ? config : {}
     }
 
@@ -681,10 +682,6 @@ class Tooltip {
         .map((token) => token.trim())
         .forEach((tClass) => tip.classList.remove(tClass))
     }
-
-    static _getInstance(element) {
-      return Data.getData(element, DATA_KEY)
-    }
   }
 
   _handlePopperPlacementChange(popperData) {
@@ -732,6 +729,10 @@ class Tooltip {
         data[config]()
       }
     })
+  }
+
+  static _getInstance(element) {
+    return Data.getData(element, DATA_KEY)
   }
 }
 
